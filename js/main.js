0f63f8be799b4698f081ad8e4a7067f072bbeb95
Mariana Marquez <marianamarquez.cs@gmail.com>
2021-04-18 14:04:56 -0300

    Commit inicial

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+alert('¡Hola! Bienvenido a Coder House');
+let cursoElegido = prompt('¿Por cuál curso querés comenzar?');
+let nombreEstudiante = prompt('Ingresa tu Nombre y Apellido');
+let dniEstudiante = Number(prompt('Ingresa tu número de DNI para la factura'));
+
+alert('¡Felicidades ' + nombreEstudiante + ', tu inscripción está completa!');
+document.write('¡Nos vemos pronto!')
