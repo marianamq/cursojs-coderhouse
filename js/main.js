d7356874ca07b582e53af4ab185d7e9be999280d
Mariana Marquez <marianamarquez.cs@gmail.com>
2021-04-18 14:33:54 -0300

    Clase 3 - Ciclos/Iteraciones

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
+let nroCliente = 6;
 
-alert('¡Hola! Bienvenido a Coder House');
-let cursoElegido = prompt('¿Por cuál curso querés comenzar?');
-let nombreEstudiante = prompt('Ingresa tu Nombre y Apellido');
-let dniEstudiante = Number(prompt('Ingresa tu número de DNI para la factura'));
-
-alert('¡Felicidades ' + nombreEstudiante + ', tu inscripción está completa!');
-document.write('¡Nos vemos pronto!')
+for(let i = 1; i < nroCliente; i++){
+    let nombreDelCliente = prompt ('Bienvenido a la estética BeautyCenter, ¿nos indicas tu nombre?');
+    let diaDelTurno = prompt('Ingresa el día de la semana que quéres tu turno'); 
+    let numeroTelefonico = Number(prompt('Ingresa tu número de telf para contactarte'));
+    if (numeroTelefonico > 10){
+        alert('¡Listo, ' + nombreDelCliente + '¡ Reservaste tu turno con éxito');
+        console.log("Cliente Nro " + i + " Registrado con éxito");
+    } else{
+        alert('Lo siento, intenta registrarte de nuevo');
+        console.log("Error al registrar usuario Nro" + i);
+    }
+}
