20b4df994710accbcf3b1114184e44a1ea8cd5a1
Mariana Marquez <marianamarquez.cs@gmail.com>
2021-05-03 23:27:19 -0300

    Clase 7 - DOM

--- js/main.js
+++ js/main.js
@@ -4,7 +4,10 @@ let totalCarrito = 0;
 let porcentajeDeDescuento = 15;
 let resultadoTotal = 0;
 let porcentajeIVA = 1.21;
-const btn = document.getElementById('btn');
+const contenedorProductos = document.getElementById('productosCard')
+const product = articulosDisponibles[0]
+const selectFiltro = document.getElementById('filtros')
+const carrito = [0]
 
 class Articulo {
   constructor (stock, nombre, precio, resenia, image) {
@@ -65,39 +68,50 @@ class Articulo {
 
 
 
+ function mostrarArticulos(array){
 
+    contenedorProductos.innerHTML = ''
 
-articulosDisponibles.push(new Articulo(3, "REMERA ZARA", 650, "Remera blanca con estampado marca Zara, como nueva. Talle: S.", "/img/remera_zara.jpg"));
-articulosDisponibles.push(new Articulo(1, "SHORT ENGOMADO", 1720, "Short engomado tiro alto con botones, como nuevo. Talle: 38.", "/img/short_engomado.jpg"));
-articulosDisponibles.push(new Articulo(2, "CARMPERA CORDERITO", 2100, "Campera de jean de hombre con interior de corderito, sin uso. Talle 40", "/img/campera_jean.jpg"));
-articulosDisponibles.push(new Articulo(0, "BUZO CON COLORES", 1370, "Buzo de dama con degradado en dos colores, con detalles en las mangas. Talle único", "/img/buzo_colores.jpg"));
-articulosDisponibles.push(new Articulo(1, "REMERA LONDON", 490, "Remera nacional de dama, como nueva. Talle: M.", "/img/remera_london.jpg"));
-articulosDisponibles.push(new Articulo(3, "JEAN H&M", 1650, "Jean con roturas H&M, con detalles en el ruedo. Talle: 44.", "/img/jean_h&m.jpg"));
+    array.forEach( (articulo) => {
+     const div = document.createElement('div')
+     div.classList.add('product')
+     div.innerHTML = `
+     <div class="card">
+       <a href="#"><img class="card-img-top" src="${articulo.image}" alt=""></a>
+       <div class="card-body">
+         <h4 class="card-title">
+           <a href="#">${articulo.nombre}</a>
+         </h4>
+         <h5>$${articulo.precio}</h5>
+         <p class="card-text">${articulo.descrip}</p>
+       </div>
+       <div class="card-footer">
+       <button onclick=agregarAlCarrito(${articulo.id})>Agregar al Carrito</button>
+         <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+       </div>
+     </div>`
+   
+     contenedorProductos.appendChild(div)
+    })
+}
+ 
 
-articulosDisponibles.forEach((articulo)=>{
-  acumuladorCardsHome += `<div class="col-lg-4 col-md-6 mb-4">
-  <div class="card h-100">
-    <a href="#"><img class="card-img-top" src="${articulo.image}" alt=""></a>
-    <div class="card-body">
-      <h4 class="card-title">
-        <a href="#">${articulo.nombre}</a>
-      </h4>
-      <h5>$${articulo.precio}</h5>
-      <p class="card-text">${articulo.resenia}</p>
-    </div>
-    <div class="card-footer">
-    <button id="btn">Agregar al Carrito</button>
-      <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-    </div>
-  </div>
-</div>`;
-});
+function filtrar(){
+  let valorFiltro = selectFiltro.value 
 
-console.log(articulosDisponibles)
+  if(valorFiltro == 'all'){
+    mostrarArticulos(stockActual)
+  } else{
+    mostrarArticulos(stockActual.filter( ele => ele.articulo === selectFiltro.value ))
+  }
+  
+}
 
-
-document.getElementById("productosCard").innerHTML = acumuladorCardsHome;
-
+function agregarAlCarrito(id){
+  let articuloElegido = stockActual.find(ele => ele.id == id)
+  carrito.push(articuloElegido)
+  console.log(carrito)
+}
 
 
 
